9b6b696c49fb1e8bfce514d308d5812f14e6bf6b
Lucas Simão <lucas.simao@trt22.jus.br>
2020-01-13 16:50:34 -0300

    configuring react-apollo library

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom";
 import { ApolloClient } from "apollo-client";
 import { HttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloProvider } from "react-apollo";
 
 import "./index.css";
 import App from "./App";
@@ -23,7 +24,12 @@ const client = new ApolloClient({
   cache
 });
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
